c30fb0c3373a676b9f51cbc3e8468402487f1310
samuelhackwill <hackmew@gmail.com>
2024-04-29 17:48:32 +0200

    Add fake pointer on the client side to monitor mouse position

--- client/logger.js
+++ client/logger.js
@@ -1,14 +1,24 @@
 import "./logger.html"
+import { ReactiveDict } from "meteor/reactive-dict"
 
 oldTimeStamp = 0
 
 Template.logger.onCreated(function () {
   // console.log(this.data.number)
+  pointer = new ReactiveDict()
+  pointer.set("X", 1)
+  pointer.set("Y", 1)
 })
 
 Template.logger.helpers({
   screenNumber() {
     return this.number
+  },
+  posX() {
+    return pointer.get("X")
+  },
+  posY() {
+    return pointer.get("Y")
   },
 })
 
@@ -16,14 +26,21 @@ Template.logger.events({
   "mousemove .container"(event) {
     // check if it has fired in the past 500ms
 
-    if (event.timeStamp < oldTimeStamp + 250) {
+    if (event.timeStamp < oldTimeStamp + 100) {
       console.log("THROTTLED!")
       return
     } else {
       oldTimeStamp = event.timeStamp
       console.log("movin, timestamp = ", event.timeStamp)
       console.log("movin, X = ", event.originalEvent.clientX, "; Y = ", event.originalEvent.clientY)
-      message = { pointer: Number(this.number), coords: [event.originalEvent.clientX, event.originalEvent.clientY] }
+
+      coordX = event.originalEvent.clientX
+      coordY = event.originalEvent.clientY
+
+      pointer.set("X", coordX)
+      pointer.set("Y", coordY)
+
+      message = { pointer: Number(this.number), coords: [coordX, coordY] }
       sendMessage(message)
     }
   },
