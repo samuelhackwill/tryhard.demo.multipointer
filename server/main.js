65c9e0af09cbbd62722017e9872e00b5ffcb2fec
ker0chan <landais.diane@gmail.com>
2024-10-01 17:49:01 +0200

    Refactored the pointer identification and movement logic

--- server/main.js
+++ server/main.js
@@ -6,6 +6,8 @@ import { streamer } from "../both/streamer.js"
 export const Clients = new Mongo.Collection("clients")
 
 const description = "Test de performance de la connection DDP. On teste d'utiliser la réactivité meteor out of the box pour voir si c'est crédible avec x pointeurs de souris en temps réél."
+let eventQueue = [];
+let pointers = [];
 
 WebApp.connectHandlers.use("/api/hello", (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*") // Allow all origins (use specific domains for more security)
@@ -18,11 +20,12 @@ WebApp.connectHandlers.use("/api/hello", (req, res, next) => {
 streamer.allowRead("all")
 streamer.allowWrite("all")
 
-streamer.on("message", function (message) {
-  // console.log("getting update : ", message)
+streamer.on("pointerMessage", function (message) {
+  if(message.type == "move") {
+    eventQueue.push(message);
+  }
 })
-Meteor.publish("clients", function () {
-  return Clients.find()
+
 })
 
 Meteor.methods({
@@ -43,6 +46,10 @@ Meteor.methods({
 Meteor.startup(async () => {
   console.log("nuking all clients ")
   Clients.remove({})
+  pointers = [];
+  eventQueue = [];
+ 
+  Meteor.setInterval(step, 1/60.0 * 1000);
 })
 
 // Automatically handle cleanup when a client disconnects
@@ -57,3 +64,37 @@ Meteor.onConnection((connection) => {
     console.log("remaining clients ", Clients.find({}).fetch())
   })
 })
+
+function step() {
+  //Process the event queue
+  eventQueue.forEach(e => {
+    let pointer = pointers.find(p => p.id == e.loggerId);
+    
+    //We don't know this pointer yet.
+    //Welcome!
+    if(pointer == undefined) {
+      pointer = createPointer(e.loggerId);
+    }
+    
+    //TODO: Rewrite this using a single vector-type param (eg pointer.move(e.coords))
+    pointer.coords.x += e.coords.x;
+    pointer.coords.y += e.coords.y;
+    pointer.dirty = true;
+  });
+  eventQueue = [];
+
+
+  let updateInstructions = {
+    pointers: pointers.filter(p => p.dirty)
+  }
+
+  streamer.emit("displayMessage", updateInstructions);
+
+  pointers = pointers.map(p => {p.dirty = false; return p;})
+} 
+
+function createPointer(id) {
+  let newPointer = { id: id, coords:{x:50, y:50} };
+  pointers.push(newPointer);
+  return newPointer;
+}
